df1ad193b50f63141964cb8eff826cfb273e4421
kristentan1 <ktan1@stevens.edu>
2018-09-27 20:09:53 -0400

    Uploading Hw04a and TestHw04a

--- Hw04a.py
+++ Hw04a.py
@@ -18,12 +18,29 @@ def getGitHubInfo(gitHubUserId):
     gitHubInfo = requests.get(urlString)
     gitHubInfoJson = json.loads(gitHubInfo.content.decode('utf-8'))
     for repo in gitHubInfoJson:
-        gitCommits = requests.get("https://api.github.com/repos/" + gitHubUserId + "/" + repo['name'] + "/commits")
+        gitCommits = requests.get("https://api.github.com/repos/" + gitHubUserId + "/" + repo["name"] + "/commits")
         gitCommitsJson = json.loads(gitCommits.content.decode('utf-8'))
         count = 0
         for commitItem in gitCommitsJson:
             count += 1
-        resultList.append([repo['name'], count])
+        resultList.append([repo["name"], count])
     return resultList #repos will be alphabetical
 
-# Adding a comment to attempt to fix error w Travis CI
+def getGitHubInfoModified(gitHubUserId):
+    """Takes in a username for a GitHub user and returns a list of their repos and the number of commits per repo."""
+    try:
+        urlString =  "https://api.github.com/users/" + gitHubUserId + "/repos"
+    except TypeError as error:
+        return "gitHubUserId must be a string"
+    resultList = []
+#     gitHubInfo = requests.get(urlString).json().decode('utf-8')
+    gitHubInfo = requests.get(urlString)
+#     gitHubInfoJson = json.loads(gitHubInfo.content.decode('utf-8'))
+    for repo in gitHubInfo: # Changed to gitHubInfo instead of gitHubInfoJson
+        gitCommits = requests.get("https://api.github.com/repos/" + gitHubUserId + "/" + repo['name'] + "/commits")
+#         gitCommitsJson = json.loads(gitCommits.content.decode('utf-8'))
+        count = 0
+        for commitItem in gitCommits: # Changed to gitCommits instead of gitCommitsJson
+            count += 1
+        resultList.append([repo['name'], count])
+    return resultList #repos will be alphabetical
--- TestHw04a.py
+++ TestHw04a.py
@@ -6,37 +6,113 @@ I pledge my Honor that I have abided by the Stevens Honor System. Kristen Tan
 '''
 
 import unittest
+# import unittest.mock
+# from unittest.mock import patch
+from unittest import mock
+from unittest.mock import Mock
 from Hw04a import getGitHubInfo
+
+class DummyObject(object):
+    def __init__(self, content):
+        self.content = content
 
 class TestHw04a(unittest.TestCase):
         
-    def testValidInput1(self):
-        self.assertEqual(getGitHubInfo('bsb226'), [['GuessingGame', 3], ['GuessingGame2', 2], ['hello-world', 3], ['HelloJava', 4], ['ud851-Exercises', 30]],
-                         'bsb226 has the following repos and commits: GuessingGame - 3, GuessingGame2 - 2, hello-world - 3, HelloJava - 4, ud851-Exercises - 30.')
+    @mock.patch('requests.get')
+    def testValidInput1(self, mockedReqs):
+        mockedResponses = [0, 0, 0, 0, 0, 0]
+        # Each nested dictionary below is like an individual repo
+        mockedResponses[0] = DummyObject(b'[{"id" : "32808844", "name" : "GuessingGame"}, {"id" : "32816276", "name" : "GuessingGame2"}, {"id" : "31634961", "name" : "hello-world"}, {"id" : "31636381", "name" : "HelloJava"}, {"id" : "134432844", "name" : "ud851-Exercises"}]')
+        mockedResponses[1] = DummyObject(b'[{"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}]')
+        mockedResponses[2] = DummyObject(b'[{"author" : "bsb226"}, {"author" : "bsb226"}]')
+        mockedResponses[3] = DummyObject(b'[{"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}]')
+        mockedResponses[4] = DummyObject(b'[{"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}]')
+        mockedResponses[5] = DummyObject(b'[{"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}, {"author" : "bsb226"}]')
         
-    def testValidInput2(self):
+        mockedReqs.side_effect = mockedResponses
+        self.assertEqual(getGitHubInfo("bsb226"), [['GuessingGame', 3], ['GuessingGame2', 2], ['hello-world', 3], ['HelloJava', 4], ['ud851-Exercises', 30]],
+                         'bsb226 has the following repos and commits: GuessingGame - 3, GuessingGame2 - 2, hello-world - 3, HelloJava - 4, ud851-Exercises - 30.')
+    
+    @mock.patch('requests.get')
+    def testValidInput2(self, mockedReqs):
+        mockedResponses = [0, 0, 0, 0, 0]
+        mockedResponses[0] = DummyObject(b'[{"id" : "28765791", "name" : "hellogitworld"}, {"id" : "144656027", "name" : "helloworld"}, {"id" : "28765763", "name" : "Project1"}, {"id" : "7468415", "name" : "threads-of-life"}]')
+        mockedResponses[1] = DummyObject(b'[{"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}, {"author" : "richkempinski"}]')
+        mockedResponses[2] = DummyObject(b'[{"author" : "richkempinski"}, {"author" : "richkempinski"}]')
+        mockedResponses[3] = DummyObject(b'[{"author" : "richkempinski"}, {"author" : "richkempinski"}]')
+        mockedResponses[4] = DummyObject(b'[{"author" : "richkempinski"}]')
+        
+        mockedReqs.side_effect = mockedResponses
         self.assertEqual(getGitHubInfo('richkempinski'), [['hellogitworld', 30], ['helloworld', 2], ['Project1', 2], ['threads-of-life', 1]], 
                          'richkempinski has the folllowing repos and commits: hellogitworld - 30, helloworld - 2, Project1 - 2, threads-of-life - 1.')
+         
+    @mock.patch('requests.get')
+    def testValidInput3(self, mockedReqs):
+        mockedResponses = [0, 0, 0]
+        mockedResponses[0] = DummyObject(b'[{"id" : "134514223", "name" : "LaundryDetector"}, {"id" : "128850081", "name" : "uunite"}]')
+        mockedResponses[1] = DummyObject(b'[{"author" : "Simoa33"}, {"author" : "Simoa33"}]')
+        mockedResponses[2] = DummyObject(b'[{"author" : "Simoa33"}, {"author" : "Simoa33"}, {"author" : "Simoa33"}, {"author" : "Simoa33"}, {"author" : "Simoa33"}, {"author" : "Simoa33"}, {"author" : "Simoa33"}, {"author" : "Simoa33"}]')
         
-    def testValidInput3(self):
+        mockedReqs.side_effect = mockedResponses
         self.assertEqual(getGitHubInfo('Simoa33'), [['LaundryDetector', 2], ['uunite', 8]], 
                          'Simoa33 has the folllowing repos and commits: LaundryDetector - 2, uunite - 8.')
-        
+
+#     @mock.patch('requests.get')
+#     def testInvalidInput1(self, mockedReq):
+#         # The mock never actually gets called, as the exception handling returns before the API call
+#         mockedReq.return_value = 'gitHubUserId must be a string'
+#         self.assertEqual(getGitHubInfo(77), 'gitHubUserId must be a string', 'Only a string may be passed to getGitHubInfo().')
+#         
+#     @mock.patch('requests.get')
+#     def testInvalidInput2(self, mockedReq):
+#         # The mock never actually gets called, as the exception handling returns before the API call
+#         mockedReq.return_value = 'gitHubUserId must be a string'
+#         self.assertEqual(getGitHubInfo(False), 'gitHubUserId must be a string', 'Only a string may be passed to getGitHubInfo().')
+#            
+#     @mock.patch('requests.get')
+#     def testInvalidInput3(self, mockedReq):
+#         # The mock never actually gets called, as the exception handling returns before the API call
+#         mockedReq.return_value = 'gitHubUserId must be a string'
+#         self.assertEqual(getGitHubInfo(9.5), 'gitHubUserId must be a string', 'Only a string may be passed to getGitHubInfo().')
+#            
+#     @mock.patch('requests.get')
+#     def testInvalidInput4(self, mockedReq):
+#         # The mock never actually gets called, as the exception handling returns before the API call
+#         mockedReq.return_value = 'gitHubUserId must be a string'
+#         self.assertEqual(getGitHubInfo([1, 2, 3, 4, 5]), 'gitHubUserId must be a string', 'Only a string may be passed to getGitHubInfo().')
+#            
+#     @mock.patch('requests.get')
+#     def testInvalidInput5(self, mockedReq):
+#         # The mock never actually gets called, as the exception handling returns before the API call
+#         mockedReq.return_value = 'gitHubUserId must be a string'
+#         self.assertEqual(getGitHubInfo((1, 2, 3, 4, 5)), 'gitHubUserId must be a string', 'Only a string may be passed to getGitHubInfo().')
+#    
+#     @mock.patch('requests.get')
+#     def testInvalidInput6(self, mockedReq):
+#         # The mock never actually gets called, as the exception handling returns before the API call
+#         mockedReq.return_value = 'gitHubUserId must be a string'
+#         self.assertEqual(getGitHubInfo({'kristen':'junior', 'kevin':'freshman'}), 'gitHubUserId must be a string', 'Only a string may be passed to getGitHubInfo().')
+         
+         
+         
+         
+# The below tests don't actually need mocking, but I wrote them using mocking above
+         
     def testInvalidInput1(self):
         self.assertEqual(getGitHubInfo(77), 'gitHubUserId must be a string', 'Only a string may be passed to getGitHubInfo().')
-        
+          
     def testInvalidInput2(self):
         self.assertEqual(getGitHubInfo(False), 'gitHubUserId must be a string', 'Only a string may be passed to getGitHubInfo().')
-        
+          
     def testInvalidInput3(self):
         self.assertEqual(getGitHubInfo(9.5), 'gitHubUserId must be a string', 'Only a string may be passed to getGitHubInfo().')
-        
+          
     def testInvalidInput4(self):
         self.assertEqual(getGitHubInfo([1, 2, 3, 4, 5]), 'gitHubUserId must be a string', 'Only a string may be passed to getGitHubInfo().')
-        
+          
     def testInvalidInput5(self):
         self.assertEqual(getGitHubInfo((1, 2, 3, 4, 5)), 'gitHubUserId must be a string', 'Only a string may be passed to getGitHubInfo().')
-
+  
     def testInvalidInput6(self):
         self.assertEqual(getGitHubInfo({'kristen':'junior', 'kevin':'freshman'}), 'gitHubUserId must be a string', 'Only a string may be passed to getGitHubInfo().')
 
